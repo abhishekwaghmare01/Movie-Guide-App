8be6409fc39be543090c611cb60877cd5e95c005
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2025-01-01 20:50:07 +0530

    function to fetch movie details using OMDB API

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ const searchForm = document.querySelector('form');
 const movieContainer = document.querySelector('.movie-container');
 const inputBox = document.querySelector('.inputBox');
 
+//function to fetch movie details using OMDB API
+const getMovieInfo = async (movie)=>{
+    const myApiKey = "e5f18a39";
+    const url = `https://www.omdbapi.com/?apikey=${myApiKey}& t=${movie}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    // console.log(data);
+    showMovieData(data);
+    
+}
+
 // Adding event listner to search form 
 searchForm.addEventListener('submit', (e)=>{
     e.preventDefault();
